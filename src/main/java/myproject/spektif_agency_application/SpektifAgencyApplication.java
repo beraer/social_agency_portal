ce06e8f7d8e4dbbe9a5bad1c78a92a98f1585203
beraer <s29315@pjwstk.edu.pl>
2025-04-16 09:58:12 +0200

    initial commit

---
+++ src/main/java/myproject/spektif_agency_application/SpektifAgencyApplication.java
@@ -0,0 +1,13 @@
+package myproject.spektif_agency_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpektifAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpektifAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myproject/spektif_agency_application/SpektifAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.spektif_agency_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpektifAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
