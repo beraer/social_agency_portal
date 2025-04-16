14f6a0967d43b550e3fc48efb30e9aa5aef2659e
beraer <s29315@pjwstk.edu.pl>
2025-04-16 11:37:28 +0200

    User entity and UserRole were added

---
+++ src/main/java/myproject/spektif_agency_application/entity/User.java
@@ -0,0 +1,18 @@
+package myproject.spektif_agency_application.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
---
+++ src/main/java/myproject/spektif_agency_application/entity/UserRole.java
@@ -0,0 +1,5 @@
+package myproject.spektif_agency_application.entity;
+
+public enum UserRole {
+    ADMIN, EMPLOYEE, CLIENT
+}
